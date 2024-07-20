e4b67ff1b7cff1c4fb839edd04c35cce7e841bf6
Devin Runowski <devinmrunowski@gmail.com>
2024-07-20 12:43:16 -0400

    add brief intro to home page with mobile,tabelt, and desktop breakpoint styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import BriefIntro from '../Components/BriefIntro/BriefIntro'
 
 function Home() {
   return (
     <main>
       <Hero />
+      <BriefIntro />
     </main>
   )
 }
